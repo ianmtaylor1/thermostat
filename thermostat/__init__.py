62ad3ff93bb85f8c08fd33e61ff8a020d19d1987
Ian Taylor <ian.m.taylor1@gmail.com>
2015-06-28 22:05:59 -0400

    Merged all sensor model classes into the sensor.py file.

---
+++ thermostat/__init__.py
@@ -0,0 +1,6 @@
+from .sensor import Base
+from .sensor import SensorGroup
+from .sensor import Sensor
+from .sensor import Reading
+from .sensor import Accuweather
+from .sensor import W1Therm
--- thermostat/sensor.py
+++ thermostat/sensor.py
@@ -1,15 +1,102 @@
-from sqlalchemy import Column, ForeignKey
-from sqlalchemy import Integer, String
+from sqlalchemy import Column, Sequence, ForeignKey
+from sqlalchemy import Integer, String, DateTime, Float
+from sqlalchemy import desc
+from sqlalchemy.orm import relationship, backref
+from sqlalchemy.ext.declarative import declarative_base
+
+from datetime import datetime
+
 from urllib.parse import urlencode
 from urllib.request import urlopen
 import xml.etree.ElementTree as ET
 import re
-from datetime import datetime
+
 from w1thermsensor.core import W1ThermSensor,NoSensorFoundError
 
-from .core import Sensor, Reading
+"""Base class for use in this module."""
+Base = declarative_base()
+
+class SensorGroup(Base):
+    """Class defining an organizational unit of sensors."""
+    __tablename__ = 'sensorgroup'
+    
+    id = Column(Integer, Sequence('sensorgroup_id_seq'), primary_key=True)
+    name = Column(String(32), nullable=False)
+    description = Column(String(100))
+    
+    sensors = relationship("Sensor",order_by="Sensor.id",backref="group")    
+    
+    def __init__(self, name, description=None):
+        self.name = name
+        self.description = description
+    
+    def __repr__(self):
+        return "<SensorGroup('{0}','{1}')>".format(self.name,self.description)
+
+class Sensor(Base):
+    """Base class of a sensor. Specific sensor types should subclass
+    this class and override the stubbed methods."""
+    __tablename__ = 'sensor'
+    
+    id = Column(Integer, Sequence('sensor_id_seq'), primary_key=True)
+    name = Column(String(32), nullable=False)
+    description = Column(String(100))
+    group_id = Column(Integer, ForeignKey('sensorgroup.id'))
+    sensortype = Column(String(32), nullable=False)
+    
+    __mapper_args__ = {'polymorphic_on':sensortype}
+    
+    def __init__(self, name, description=None):
+        self.name = name
+        self.description = description
+    
+    def __repr__(self):
+        return "<Sensor('{0}','{1}','{2}','{3}')>".format(
+            self.name,
+            self.description,
+            self.sensortype,
+            self.group.name)
+    
+    def available(self):
+        """Returns a boolean value indicating whether this sensor can be read.
+        """
+        raise NotImplementedError()
+    
+    def read(self):
+        """Return a Reading instance representing the current temperature
+        from this sensor."""
+        raise NotImplementedError()
+    
+
+class Reading(Base):
+    """Defines a temperature reading taken by a sensor at a specific time."""
+    __tablename__ = 'reading'
+    
+    id = Column(Integer, Sequence('reading_id_seq'), primary_key=True)
+    sensor_id = Column(Integer, ForeignKey('sensor.id'), nullable=False)
+    time = Column(DateTime, nullable=False)
+    value = Column(Float, nullable=False)
+    
+    sensor = relationship('Sensor',
+                backref=backref('readings',
+                    order_by='desc(Reading.time)',
+                    lazy='dynamic'))
+    
+    def __init__(self,time,value,sensor):
+        self.time = time
+        self.value = value
+        self.sensor = sensor
+    
+    def __repr__(self):
+        return "<Reading({0},'{1}',{2:5.1f})>".format(
+            repr(self.sensor),
+            self.time.strftime('%Y-%m-%d %H:%M:%S'),
+            self.value)
+
 
 class Accuweather(Sensor):
+    """A sensor class representing an outdoor temperature 
+    fetched from Accuweather."""
     __tablename__ = 'accuweather'
     __mapper_args__ = {'polymorphic_identity':'accuweather'}
     
@@ -46,6 +133,10 @@ class Accuweather(Sensor):
 
 
 class W1Therm(Sensor):
+    """A sensor class representing a 1-wire temperature sensor,
+    e.g. DS18b20 
+    Uses the w1thermsensor package for the heavy lifting.
+    Source: https://github.com/timofurrer/w1thermsensor"""
     __tablename__ = 'w1therm'
     __mapper_args__ = {'polymorphic_identity':'w1therm'}
 
