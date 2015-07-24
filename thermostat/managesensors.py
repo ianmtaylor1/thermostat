600ce712201e1c0c9bc5752d2540f4d2bf909f03
Ian Taylor <ian.m.taylor1@gmail.com>
2015-07-24 00:38:08 -0400

    Switched to argparse subcommands. Implemented adding accuweather sensors.

--- thermostat/managesensors.py
+++ thermostat/managesensors.py
@@ -11,7 +11,7 @@ from thermostat.config import Config
 
 Session = sessionmaker()
 
-def listsensors():
+def listsensors(args):
     """Get list of all sensors"""
     session = Session()
     groups = session.query(SensorGroup).all()
@@ -35,23 +35,44 @@ def listsensors():
             print("    id={0} '{1}' {2}".format(s.id,s.name,available))
     if foundsensors==False:
         print("No sensors or groups.")
-    return 0
 
-def addsensor():
-    """Add a sensor to the database."""
-    raise NotImplementedError('Adding sensor not implemented')
-
+def add_accuweather(args):
+    """Add an accuweather sensor to the database."""
+    sensor = Accuweather(args.zipcode,name=args.name,description=args.description)
+    session = Session()
+    confirmformat = "Name: {0}\nDescription: {1}\nLocation Code: {2}\n"
+    print(confirmformat.format(sensor.name,sensor.description,sensor.loccode))
+    if input("Add this sensor? [Y/N] ").lower() == 'y':
+        session.add(sensor)
+        session.commit()
+    else:
+        print("Abort.")
+    
+def add_w1therm(args):
+    """Add a W1Therm sensor to the database."""
+    raise NotImplementedError('Adding W1Therm sensor not implemented.')
 
 def main(): 
     """Main entry point."""
     # Set up command line parser
     parser = argparse.ArgumentParser()
-    parser.add_argument('command',choices=['list','addsensor'],
-                        help='Command to run')
-    parser.add_argument('-c','--config',help='Configuration file path',
-                        default='thermostat.conf',dest='configfile',
-                        metavar='filename')
+    parser.add_argument('-c','--config',help='Configuration file path',default='thermostat.conf',dest='configfile',metavar='filename')
+    main_subparsers = parser.add_subparsers()
+    parser_list = main_subparsers.add_parser('list',description='List all sensors organized by group',help='List all sensors organized by group')
+    parser_list.set_defaults(func=listsensors)
+    parser_add = main_subparsers.add_parser('addsensor',description='Add a sensor to the database',help='Add a sensor')
+    parser_add.add_argument('-n','--name')
+    parser_add.add_argument('-d','--description')
+    add_subparsers = parser_add.add_subparsers(help='Type of sensor to add')
+    parser_aw = add_subparsers.add_parser('accuweather',description='Add an Accuweather sensor to the database',help='Add Accuweather sensor')
+    parser_aw.add_argument('zipcode',help='ZIP Code for Accuweather sensor')
+    parser_aw.set_defaults(func=add_accuweather)
+    parser_w1 = add_subparsers.add_parser('w1therm',description='Add a W1Therm sensor to the database',help='Add W1Therm sensor')
+    parser_w1.add_argument('type',type=int)
+    parser_w1.add_argument('id')
+    parser_w1.set_defaults(func=add_w1therm)
     args = parser.parse_args()
+
     # Get the config file name from command line
     defaultfile = 'thermostat.conf.defaults'
     config = Config(args.configfile,defaultfile)
@@ -61,10 +82,5 @@ def main():
     engine = create_engine(cxn,echo=echo)
     Session.configure(bind=engine)
     # Check what the command is and call appropriate function
-    if args.command == 'list':
-        return listsensors()
-    elif args.command == 'addsensors':
-        return addsensor()
-    else:
-        return 255
-
+    args.func(args)
+    return 0
