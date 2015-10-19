6e8cfdd7555b0e9005f63e7d61e9b4d7ccd65e10
Ian Taylor <ian.m.taylor1@gmail.com>
2015-10-18 20:36:39 -0400

    Added readsensor option to manage-sensors program

--- thermostat/managesensors.py
+++ thermostat/managesensors.py
@@ -103,6 +103,26 @@ def change_group(args):
     else:
         print("Abort.")
 
+def readsensor(args):
+    """Read the supplied sensor and output the result."""
+    session = Session()
+    # Find the sensor and read it
+    sensorquery = session.query(Sensor).filter_by(id=args.sensorid)
+    if sensorquery.count()==0:
+        raise Exception('No sensors found with id={0}'.format(args.sensorid))
+    elif sensorquery.count()>1:
+        raise Exception('Multiple sensors found with id={0}'.format(args.sensorid))
+    sensor = sensorquery.all()[0]
+    reading = sensor.read()
+    # Output the results
+    print("id={id}, '{name}'".format(id=sensor.id,name=sensor.name))
+    print("Time: {dt}".format(dt=reading.time.strftime('%Y-%m-%d %H:%M:%S')))
+    print("Value: {v:4.1f}".format(v=reading.value))
+    # If 'save' is set, save the reading
+    if args.save==True:
+        session.commit()
+        print("Saved to database.")
+
 
 def main(cmdline=None):
     """Main entry point."""
@@ -111,27 +131,39 @@ def main(cmdline=None):
     parser.add_argument('-c','--config',help='Configuration file path',default='thermostat.conf',dest='configfile',metavar='filename')
     parser.add_argument('-y','--yes',action='store_true',help='Answer "yes" to all prompts')
     main_subparsers = parser.add_subparsers()
+    
     parser_list = main_subparsers.add_parser('list',description='List all sensors organized by group',help='List all sensors organized by group')
     parser_list.set_defaults(func=listsensors)
+    
     parser_add = main_subparsers.add_parser('addsensor',description='Add a sensor to the database',help='Add a sensor')
     parser_add.add_argument('-n','--name')
     parser_add.add_argument('-d','--description')
     add_subparsers = parser_add.add_subparsers(help='Type of sensor to add')
+    
     parser_aw = add_subparsers.add_parser('accuweather',description='Add an Accuweather sensor to the database',help='Add Accuweather sensor')
     parser_aw.add_argument('zipcode',help='ZIP Code for Accuweather sensor')
     parser_aw.set_defaults(func=add_accuweather)
+    
     parser_w1 = add_subparsers.add_parser('w1therm',description='Add a W1Therm sensor to the database',help='Add W1Therm sensor')
     parser_w1.add_argument('type',type=int)
     parser_w1.add_argument('id')
     parser_w1.set_defaults(func=add_w1therm)
+    
     parser_addgroup = main_subparsers.add_parser('addgroup',description='Add a sensor group to the database',help='Add a sensor group')
     parser_addgroup.add_argument('name')
     parser_addgroup.add_argument('-d','--description')
     parser_addgroup.set_defaults(func=add_group)
+    
     parser_changegroup = main_subparsers.add_parser('changegroup',description="Change a sensor's assigned group. Leave groupid blank to remove its group.",help="Change a sensor's group")
     parser_changegroup.add_argument('sensorid',type=int,help='ID of the sensor to modify')
     parser_changegroup.add_argument('groupid',nargs='?',default=None,type=int,help="ID of the sensor's new group (blank for none)")
     parser_changegroup.set_defaults(func=change_group)
+    
+    parser_readsensor = main_subparsers.add_parser('readsensor',description="Take a reading from a sensor",help="Read a sensor's value")
+    parser_readsensor.add_argument('sensorid',type=int,help='ID of the sensor to be read')
+    parser_readsensor.add_argument('-s','--save',action='store_true',help='Save the reading to the database')
+    parser_readsensor.set_defaults(func=readsensor)
+    
     if cmdline is None:
         args = parser.parse_args()
     else:
